d8ad43b46d2a70718f20c68584de43d8920a75f0
glenn-pickett <glenndpickett@gmail.com>
2023-10-04 09:12:13 -0400

    Update index.js

--- index.js
+++ index.js
@@ -8,16 +8,16 @@ import server from './lib/server.cjs';
 //let workers = require('./lib/workers');
 import workers from './lib/workers.cjs';
 //let cli = require('./lib/cli');
-//import cli from './lib/cli.cjs';
+import cli from './lib/cli.cjs';
 
 const App = () =>{
     server.init();
     workers.init();
-    /*
+    
     setTimeout(()=>{
         cli.init();
     },50);
-    */
+    
 }
 
 //App();
